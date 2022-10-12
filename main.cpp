75855fe951686598ffb8aafc3e28471ee0520274
Rufelle Emmanuel Pactol <rufelleemmanuelpactol7@gmail.com>
2022-10-12 23:44:56 +0800

    tres lang sir

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-
+#include "fulcrum.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::fulcrum arr(5);
+    arr.enqueue(5);
+    arr.push(3);
+    arr.push(2);
+    arr.enqueue(4);
+    std::cout << arr.pop() << std::endl;
+    for (int i=0; i<arr.arrsize(); i++){
+        std::cout << arr.array[i] << std::endl;
+    }
     return 0;
 }
